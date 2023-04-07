02868fca475a5ae538bb7a8fa9c2a073c9e249e8
Choi-hyeona <xhrl523@naver.com>
2023-04-07 13:00:24 +0900

    exact 추가

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/Notice" element={<Notice />} />
-          <Route path="/Setting" element={<Setting />} />
+          <Route exact path="/" element={<Main />} />
+          <Route exact path="/Notice" element={<Notice />} />
+          <Route exact path="/Setting" element={<Setting />} />
         </Routes>
       </BrowserRouter>
     </>
