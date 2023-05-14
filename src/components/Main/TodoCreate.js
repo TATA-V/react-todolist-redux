2295edae078342cdaadacf3005794f8455408ba5
Choi-hyeona <xhrl523@naver.com>
2023-05-14 17:34:53 +0900

    readme 수정

--- src/components/Main/TodoCreate.js
+++ src/components/Main/TodoCreate.js
@@ -93,8 +93,8 @@ const ModalBg = styled.div`
   }
 
   position: absolute;
-  width: 380px;
-  height: 831px;
+  width: 100%;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.65);
   animation: opacity 0.125s linear;
 `;
